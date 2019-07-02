730bdaac816763891d900ff3e55084f87742e656
siminfei <2230988414@qq.com>
2019-07-02 11:25:12 +0800

    久久乘法表

---
+++ nine.php
@@ -0,0 +1,18 @@
+<?php
+
+
+for($i=1;$i<=9;$i++){
+    for ($j=1;$j<=$i;$j++){
+        $p=$i*$j;
+        echo "$i*$j=$p";
+        echo " ";
+
+
+    }//j   in
+    echo "<br/>";
+}//i  out
+
+
+
+
+?>
